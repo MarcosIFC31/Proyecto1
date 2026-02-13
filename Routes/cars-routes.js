81d802729fa6a50e27e5695fb5b88780c9d21311
Marcos Cano <marcoscano@paucasesnovescifp.cat>
2026-02-13 18:02:59 +0100

    Solo cambie a mayusculas

--- Routes/cars-routes.js
+++ Routes/cars-routes.js
@@ -3,5 +3,5 @@ var router = express.Router();
 
 
 const carControler = require('../Controllers/cars-controller');
-router.get('/api/listacars', carcontroler.listacars)
+router.get('/api/listacars', carControler.listacars)
 module.exports = router;
